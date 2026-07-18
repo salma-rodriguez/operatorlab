23b9281c030e204e439b5ea90e467b0750f60f5c
salma-rodriguez <srodr063@fiu.edu>
2026-07-17 21:14:50 -0400

    updating __init__ under 'stochastic_operators'

--- stochastic_operators/__init__.py
+++ stochastic_operators/__init__.py
@@ -4,6 +4,7 @@ Stochastic operator constructions and analysis tools for OperatorLab.
 
 from .ergodic import ErgodicAnalyzer
 from .generators import MarkovGenerator
+from .hitting import HittingAnalyzer
 from .markov import MarkovOperator
 from .operators import (
     StochasticConvention,
@@ -13,6 +14,7 @@ from .stationary import StationaryAnalyzer
 
 __all__ = [
     "ErgodicAnalyzer",
+    "HittingAnalyzer",
     "MarkovGenerator",
     "MarkovOperator",
     "StochasticAnalyzer",
